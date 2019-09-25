c8d934bb23d6495e23d704b11431fbd496f8b97d
JohanDenver <48187072+johandenver@users.noreply.github.com>
2019-09-25 21:53:17 +1000

    completed reversestring exercize

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,48 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// one way using reverse() function:
+// function reverse(str) {
+//   const arr = str.split("");
+//   arr.reverse();
+//   return arr.join("");
+// }
+
+// same but simplified:
+// function reverse(str) {
+//   return str
+//     .split("")
+//     .reverse()
+//     .join("");
+// }
+
+//  a second way:
+// function reverse(str) {
+//   // create empty array:
+//   let reversed = "";
+//   // create a for loop:
+//   for (let charachter of str) {
+//     reversed = charachter + reversed;
+//   }
+//   return reversed;
+// }
+
+// a third way:
+// function reverse(str) {
+//   // reduce() simplifies down to a single value:
+//   return str.split("").reduce((reversed, charachter) => {
+//     return charachter + reversed;
+//   }, "");
+// }
+
+// same but simplified:
+function reverse(str) {
+  // reduce() simplifies down to a single value:
+  return str.split("").reduce((rev, char) => char + rev, "");
+}
 
 module.exports = reverse;
